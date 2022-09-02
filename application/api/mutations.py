a551af335df5978fe5bd5f9da3cf8592ac3e2419
Teo Podobnik <teo.podobnik1234@gmail.com>
2022-09-02 12:16:30 +0200

    feat: additional characteristics of a player

--- application/api/mutations.py
+++ application/api/mutations.py
@@ -1,14 +1,66 @@
 from ariadne import convert_kwargs_to_snake_case
+import psycopg2 
 from .. import db
 from ..models import PlayerModel
-import psycopg2 
+
+def add_elem_to_string(string, elem):
+    if string is None:
+        return elem
+    return string + ',' + elem
+
+def average(x, y):
+    if x is None:
+        return y
+    elif y is None:
+        return x
+    array = x.split(',')
+    array = [eval(i) for i in array]
+    array.append(y)
+    return sum(array) / len(array)
 
 @convert_kwargs_to_snake_case
-def createPlayer_resolver(obj, info, name, votes=None):
+def createPlayer_resolver(obj, 
+                          info, 
+                          name, 
+                          votes=None,
+                          total_points=None,
+                          break_points=None,
+                          winloss=None,
+                          total_serve=None,
+                          error_serve=None,
+                          points_serve=None,
+                          total_reception=None,
+                          error_reception=None,
+                          pos_reception=None,
+                          exc_reception=None,
+                          total_attacks=None,
+                          error_attacks=None,
+                          blocked_attacks=None,
+                          points_attack=None,
+                          pos_attack=None,
+                          points_block=None,
+                          errors=None):
     try:
         player = PlayerModel(
             name=name, 
-            votes=votes
+            votes=votes,
+            total_points=total_points,
+            break_points=break_points,
+            winloss=winloss,
+            total_serve=total_serve,
+            error_serve=error_serve,
+            points_serve=points_serve,
+            total_reception=total_reception,
+            error_reception=error_reception,
+            pos_reception=pos_reception,
+            exc_reception=exc_reception,
+            total_attacks=total_attacks,
+            error_attacks=error_attacks,
+            blocked_attacks=blocked_attacks,
+            points_attack=points_attack,
+            pos_attack=pos_attack,
+            points_block=points_block,
+            errors=errors,
         )
 
         try:
@@ -33,12 +85,68 @@ def createPlayer_resolver(obj, info, name, votes=None):
     return payload
 
 @convert_kwargs_to_snake_case
-def updatePlayer_resolver(obj, info, name, votes=None):
+def updatePlayer_resolver(obj, 
+                          info,
+                          name, 
+                          votes=None,
+                          total_points=None,
+                          break_points=None,
+                          winloss=None,
+                          total_serve=None,
+                          error_serve=None,
+                          points_serve=None,
+                          total_reception=None,
+                          error_reception=None,
+                          pos_reception=None,
+                          exc_reception=None,
+                          total_attacks=None,
+                          error_attacks=None,
+                          blocked_attacks=None,
+                          points_attack=None,
+                          pos_attack=None,
+                          points_block=None,
+                          errors=None):
     try:
         player = PlayerModel.query.filter_by(name=name).first()
         if player:
             if votes is not None:
-                player.votes = votes
+                player.votes = add_elem_to_string(player.votes, votes)
+            if total_points is not None:
+                player.total_points = add_elem_to_string(player.total_points, total_points)
+                player.points_avg = average(player.total_points, player.points_avg)
+            if break_points is not None:
+                player.break_points = add_elem_to_string(player.break_points, break_points)
+            if winloss is not None:
+                player.winloss = add_elem_to_string(player.winloss, winloss)
+            if total_serve is not None:
+                player.total_serve = add_elem_to_string(player.total_serve, total_serve)
+            if error_serve is not None:
+                player.error_serve = add_elem_to_string(player.error_serve, error_serve)
+            if points_serve is not None:
+                player.points_serve = add_elem_to_string(player.points_serve, points_serve)
+            if total_reception is not None:
+                player.total_reception = add_elem_to_string(player.total_reception, total_reception)
+            if error_reception is not None:
+                player.error_reception = add_elem_to_string(player.error_reception, error_reception)
+            if pos_reception is not None:
+                player.pos_reception = add_elem_to_string(player.pos_reception, pos_reception)
+            if exc_reception is not None:
+                player.exc_reception = add_elem_to_string(player.exc_reception, exc_reception)
+            if total_attacks is not None:
+                player.total_attacks = add_elem_to_string(player.total_attacks, total_attacks)
+            if error_attacks is not None:
+                player.error_attacks = add_elem_to_string(player.error_attacks, error_attacks)
+            if blocked_attacks is not None:
+                player.blocked_attacks = add_elem_to_string(player.blocked_attacks, blocked_attacks)
+            if points_attack is not None:
+                player.points_attack = add_elem_to_string(player.points_attack, points_attack)
+            if pos_attack is not None:
+                player.pos_attack = add_elem_to_string(player.pos_attack, pos_attack)
+                player.attack_avg = average(player.pos_attack, player.attack_avg)
+            if points_block is not None:
+                player.points_block = add_elem_to_string(player.points_block, points_block)
+            if errors is not None:
+                player.errors = add_elem_to_string(player.errors, errors)
             db.session.add(player)
             db.session.commit()
             payload = {
--- application/models.py
+++ application/models.py
@@ -6,6 +6,25 @@ class PlayerModel(db.Model):
     id_player = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     votes = db.Column(db.String(1000), nullable=True)
+    total_points = db.Column(db.String(1000), nullable=True)
+    break_points = db.Column(db.String(1000), nullable=True)
+    winloss = db.Column(db.String(1000), nullable=True)
+    total_serve = db.Column(db.String(1000), nullable=True)
+    error_serve = db.Column(db.String(1000), nullable=True)
+    points_serve = db.Column(db.String(1000), nullable=True)
+    total_reception = db.Column(db.String(1000), nullable=True)
+    error_reception = db.Column(db.String(1000), nullable=True)
+    pos_reception = db.Column(db.String(1000), nullable=True)
+    exc_reception = db.Column(db.String(1000), nullable=True)
+    total_attacks = db.Column(db.String(1000), nullable=True)
+    error_attacks = db.Column(db.String(1000), nullable=True)
+    blocked_attacks = db.Column(db.String(1000), nullable=True)
+    points_attack = db.Column(db.String(1000), nullable=True)
+    pos_attack = db.Column(db.String(1000), nullable=True)
+    points_block = db.Column(db.String(1000), nullable=True)
+    errors = db.Column(db.String(1000), nullable=True)
+    points_avg = db.Column(db.Float, nullable=True)
+    attack_avg = db.Column(db.Float, nullable=True)
 
     def __repr__(self):
         return '<Player #%s>' % self.id_player
@@ -19,4 +38,23 @@ class PlayerModel(db.Model):
             'id_player': self.id_player,
             'name': self.name,
             'votes': self.votes,
+            'total_points': self.total_points,
+            'break_points': self.break_points,
+            'winloss': self.winloss,
+            'total_serve': self.total_serve,
+            'error_serve': self.error_serve,
+            'points_serve': self.points_serve,
+            'total_reception': self.total_reception,
+            'error_reception': self.error_reception,
+            'pos_reception': self.pos_reception,
+            'exc_reception': self.exc_reception,
+            'total_attacks': self.total_attacks,
+            'error_attacks': self.error_attacks,
+            'blocked_attacks': self.blocked_attacks,
+            'points_attack': self.points_attack,
+            'pos_attack': self.pos_attack,
+            'points_block': self.points_block,
+            'errors': self.errors,
+            'points_avg': self.points_avg,
+            'attack_avg': self.attack_avg
         }
