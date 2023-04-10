0effb00e1e788ed937109b0262a5d6930e258f98
Teodor Janez Podobnik <teo.podobnik1234@gmail.com>
2023-04-10 08:30:19 +0200

    integrated with PDF scraper

--- application/api/mutations.py
+++ application/api/mutations.py
@@ -4,15 +4,17 @@ from .. import db
 from ..models import *
 
 def add_elem_to_string(string, elem):
+    if string == '':
+        return elem
     return string + ',' + elem
 
 def average(x, y):
-    if x is None:
+    if x == 0:
         return y
-    elif y is None:
+    elif y == 0:
         return x
     array = x.split(',')
-    array = [eval(i) for i in array]
+    array = [int(i) for i in array]
     array.append(y)
     return sum(array) / len(array)
 
@@ -119,7 +121,7 @@ def createPlayer_resolver(obj,
             except (psycopg2.errors.UniqueViolation, sqlalchemy.exc.IntegrityError) as e:
                 payload = {
                 "success": False,
-                "errors": ["Failed to add player to the database"]
+                "errors": ["Failed to add player to the database - it's possible it already exists!"]
             }
 
     except ValueError as e:  
@@ -172,7 +174,7 @@ def updatePlayer_resolver(obj,
                 player.votes = add_elem_to_string(player.votes, votes)
             if totalPoints is not None:
                 player.totalPoints = add_elem_to_string(player.totalPoints, totalPoints)
-                player.pointsAvg = average(player.totalPoints, player.pointsAvg)
+                #player.pointsAvg = average(player.totalPoints, player.pointsAvg)
             if breakPoints is not None:
                 player.breakPoints = add_elem_to_string(player.breakPoints, breakPoints)
             if winloss is not None:
@@ -186,7 +188,7 @@ def updatePlayer_resolver(obj,
             if totalReception is not None:
                 player.totalReception = add_elem_to_string(player.totalReception, totalReception)
             if errorReception is not None:
-                player.error_reception = add_elem_to_string(player.error_reception, errorReception)
+                player.errorReception = add_elem_to_string(player.errorReception, errorReception)
             if posReception is not None:
                 player.posReception = add_elem_to_string(player.posReception, posReception)
             if excReception is not None:
@@ -201,10 +203,10 @@ def updatePlayer_resolver(obj,
                 player.pointsAttack = add_elem_to_string(player.pointsAttack, pointsAttack)
             if posAttack is not None:
                 player.posAttack = add_elem_to_string(player.posAttack, posAttack)
-                player.attackAvg = average(player.posAttack, player.attackAvg)
+                #player.attackAvg = average(player.posAttack, player.attackAvg)
             if pointsBlock is not None:
                 player.pointsBlock = add_elem_to_string(player.pointsBlock, pointsBlock)
-            if errorServe is not None and errorReception is not None and errorAttacks is not None: 
+            if errorServe not in ['.', None] and errorReception not in ['.', None] and errorAttacks not in ['.', None]: 
                 errors = str(int(errorServe) + int(errorReception) + int(errorAttacks))
                 player.errors = add_elem_to_string(player.errors, errors)
             if dateTeam is not None:
@@ -217,8 +219,8 @@ def updatePlayer_resolver(obj,
             }
         else:
             payload = {
-                "success": False,
-                "errors": [f"Stats for player {name} already saved this game"]
+                "success": True,
+                "errors": [f"WARNING: Stats for player {name} already saved this game"]
             }
     except AttributeError as e:  # todo not found
         payload = {
