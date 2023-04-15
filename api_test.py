946ca29db5561393460b5501f2e269ba762f8746
Teodor Janez Podobnik <teo.podobnik1234@gmail.com>
2023-04-15 15:54:38 +0200

    fixed players retrieval in getTeam

--- api_test.py
+++ api_test.py
@@ -5,110 +5,116 @@ from sgqlc.operation import Operation
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
-        self.BASE = "http://172.34.1.3:5000"
+        self.BASE = "http://172.35.1.3:5000"
         self.query = Operation(Query)
         self.mutation = Operation(Mutation)
 
     def test_a_create_team(self):
         self.mutation.createTeam(name='Calcit Volley')
         resp = requests.post(self.BASE + "/teams", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["createTeam"]["success"])
 
     def test_b_create_team(self):
         self.mutation.createTeam(name='ACH Volley')
         resp = requests.post(self.BASE + "/teams", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["createTeam"]["success"])
         
     def test_c_create_player(self):
         self.mutation.createPlayer(name='Mark', teamName='Calcit Volley')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["createPlayer"]["success"])
 
     def test_d_create_player(self):
         self.mutation.createPlayer(name='Teo', teamName='Calcit Volley')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["createPlayer"]["success"])
 
     def test_e_create_player(self):
         self.mutation.createPlayer(name='Klemen', teamName='ACH Volley')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["createPlayer"]["success"])
 
     def test_f_create_preexist_player(self):
         self.mutation.createPlayer(name='Klemen', teamName='ACH Volley')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertFalse(resp.json()["data"]["createPlayer"]["success"])
 
     def test_g_get_teams(self):
         self.query.getTeams()
         resp = requests.post(self.BASE + "/teams", json={'query': str(self.query)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["getTeams"]["success"])
 
     def test_h_get_player(self):
         self.query.getPlayer(name='Mark')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.query)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["getPlayer"]["success"])
       
     def test_i_get_players(self):
         self.query.getPlayers()
         resp = requests.post(self.BASE + "/players", json={'query': str(self.query)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["getPlayers"]["success"])
     
     def test_j_update_player(self):
         self.mutation.updatePlayer(name='Mark', votes='not null', dateTeam='test')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["updatePlayer"]["success"])
 
     def test_k_get_players(self):
         self.query.getPlayers()
         resp = requests.post(self.BASE + "/players", json={'query': str(self.query)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["getPlayers"]["success"])
 
-    def test_l_delete_player(self):
-        self.mutation.deletePlayer(name='Teo')
-        resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
+    def test_l_get_team(self):
+        self.query.getTeam(name="Calcit Volley")
+        resp = requests.post(self.BASE + "/teams", json={'query': str(self.query.__to_graphql__(auto_select_depth=3))})
         print(resp.json())
-        self.assertTrue(resp.json()["data"]["deletePlayer"]["success"])
+        self.assertTrue(resp.json()["data"]["getTeam"]["success"])
 
     def test_m_delete_player(self):
+        self.mutation.deletePlayer(name='Teo')
+        resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
+        #print(resp.json())
+        self.assertTrue(resp.json()["data"]["deletePlayer"]["success"])
+
+    def test_n_delete_player(self):
         self.mutation.deletePlayer(name='Mark')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["deletePlayer"]["success"])
     
-    def test_n_delete_player(self):
+    def test_o_delete_player(self):
         self.mutation.deletePlayer(name='Klemen')
         resp = requests.post(self.BASE + "/players", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["deletePlayer"]["success"])
 
-    def test_o_delete_team(self):
+    def test_p_delete_team(self):
         self.mutation.deleteTeam(name='Calcit Volley')
         resp = requests.post(self.BASE + "/teams", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["deleteTeam"]["success"])
 
-    def test_p_delete_team(self):
+    def test_r_delete_team(self):
         self.mutation.deleteTeam(name='ACH Volley')
         resp = requests.post(self.BASE + "/teams", json={'query': str(self.mutation)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["deleteTeam"]["success"])
 
-    def test_r_get_players(self):
+    def test_s_get_players(self):
         self.query.getPlayers()
         resp = requests.post(self.BASE + "/players", json={'query': str(self.query)})
-        print(resp.json())
+        #print(resp.json())
         self.assertTrue(resp.json()["data"]["getPlayers"]["success"])
 
 if __name__ == '__main__':
--- application/models.py
+++ application/models.py
@@ -13,7 +13,7 @@ class Team(db.Model):
         return {
             'id': self.id,
             'name': self.name,
-            'players': [player for player in self.players],
+            'players': [player.to_json() for player in self.players],
         }
 
 class Player(db.Model):
